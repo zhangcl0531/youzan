67555242e540dcd5bac06eff707899e49629ec26
zhangcl0531 <zclphone19940531@163.com>
2019-08-07 20:29:16 +0800

    20190807

--- youzan/src/pages/cart/cart.js
+++ youzan/src/pages/cart/cart.js
@@ -11,11 +11,52 @@ new Vue({
     el:'.container',
     data() {
         return {
-            lists:null
+            lists:null,
+            total:0,
+            editingshop:null,
+            editingshopindex: -1
         }
     },
     computed:{
+        allsetected:{
+            get(){
+                if(this.lists&&this.lists.length){
+                    return this.lists.every(shop => {
+                        return shop.checked
+                    })
+                }
+                return false
+            },
+            set(newval){
+                this.lists.forEach(shop => {
+                    shop.checked = newval
+                    shop.goodsList.forEach(good =>{
+                        good.checked = newval
+                    })
+                })
+            }
+        },
 
+        selectlists(){
+            if(this.lists&&this.lists.length){
+                let arr = []
+                let total = 0
+                this.lists.forEach(shop =>{
+                    shop.goodsList.forEach(good => {
+                        if(good.checked){
+                            arr.push(good)
+                            total += good.price * good.number
+                        }
+                    })
+                })
+                this.total = total
+                return arr
+            }
+            return []
+        },
+        removelists(){
+
+        }
     },
     created(){
         this.getlist()
@@ -27,29 +68,54 @@ new Vue({
                  let lists = res.data.cartList
                  lists.forEach(shop => {
                     shop.checked = true
+                    shop.editing = false
+                    shop.editingMsg = '编辑'
+                    shop.removechecked = false
                     shop.goodsList.forEach(good => {
                         good.checked = true
+                        good.removechecked = false
                     })
                 });
                 this.lists = lists
             })
         },
-        selectgood(good){
+        selectgood(shop,good){
             good.checked = !good.checked
+            //遍历商品列表，如果good.checked都是true则shop.checked为true
+            shop.checked = shop.goodsList.every(good =>{
+                return good.checked == true
+            })
 
         },
         selectshop(shop){
             shop.checked = !shop.checked
-            // console.log(this.lists.shop)
-            // if(shop.checked){
-            //     this.lists.shop.forEach(good=>{
-            //         good.checked = true
-            //     })
-            // }else{
-            //     this.lists.shop.forEach(good=>{
-            //         good.checked = false
-            //     })
-            // }
+            shop.goodsList.forEach(good =>{
+                good.checked = shop.checked
+            })
+        },
+        selectall(){
+            this.allsetected =!this.allsetected
+        },
+        edit(shop,shopindex){
+            shop.editing = !shop.editing
+            if(shop.editing){
+                shop.editingMsg = '完成'
+                this.editingshop = shop.editing
+                this.editingshopindex = shopindex
+            }else{
+                shop.editingMsg = '编辑'
+                this.editingshop = null
+                this.editingshopindex = -1
+            }
+            this.lists.forEach((item,i)=>{
+                if(shopindex !==i ){
+                    item.editing = false
+                    item.editingMsg = '编辑'
+                }
+            })
+            
+            
+            
         }
     },
 })
